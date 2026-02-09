852565ad11b5db481fbcef3e07082b420105742b
JSNLIE <JSNLIE@DESKTOP-RM14S9D>
2026-02-09 16:04:44 +0700

    Configured DB SQL Server And Add Model

---
+++ MiniProjectExam1/Models/Ticket.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniProjectExam1.Models
+{
+    public class Ticket
+    {
+        public string CodeTicket { get; set; }
+        public string NameTicket { get; set; }
+        public string CategoryTicket { get; set; }
+        public DateTime EventDate { get; set; }
+        public decimal Price { get; set; }
+        public int Quota { get; set; }
+        public int RemainingQuota { get; set; }
+    }
+}
